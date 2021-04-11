661445d1da36e8d4b5d3d9a5e775eee681233718
Daren Lobo <lobo.daren@gmail.com>
2021-04-11 16:34:07 +0530

    Redesign

--- js/scripts.js
+++ js/scripts.js
@@ -20,19 +20,19 @@ function reset() {
   imgObj.style.animation =  '0s ease 0s 1 normal none running none';
 }
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    // document.getElementById("navbar").style.padding = "0.9rem 4rem";
-    document.getElementById("navbar").style.opacity= "1";
-    document.getElementById("navbar").style.background = "var(--colBG)";
-  } else {
-    // document.getElementById("navbar").style.padding = "1.8rem 4rem";
-    document.getElementById("navbar").style.background = "none";
-    document.getElementById("navbar").style.opacity= "0";
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     // document.getElementById("navbar").style.padding = "0.9rem 4rem";
+//     document.getElementById("navbar").style.opacity= "1";
+//     document.getElementById("navbar").style.background = "var(--colBG)";
+//   } else {
+//     // document.getElementById("navbar").style.padding = "1.8rem 4rem";
+//     document.getElementById("navbar").style.background = "none";
+//     document.getElementById("navbar").style.opacity= "0";
 
-  }
+//   }
 }
 
 window.onload= init;
