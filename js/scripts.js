eb6926676bebe8e235f6ad18a16ce97cd642113d
Daren Lobo <lobo.daren@gmail.com>
2021-04-28 23:06:29 +0530

    responsive nav and padding

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,14 @@ function reset() {
   imgObj.style.animation =  '0s ease 0s 1 normal none running none';
 }
 
+function toggleMenu() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
 // window.onscroll = function() {scrollFunction()};
 
 // function scrollFunction() {
